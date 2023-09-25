e2c4709e384c86a372992fefbf208cd0481604e2
ritik2102 <ritik21feb@gmail.com>
2023-09-25 15:28:55 +0530

    Relations between tables

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,15 +16,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
   Product.findById(prodId)
     .then(product => {
       res.render('shop/product-detail', {
@@ -70,36 +61,41 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  let fetchedCart;
-  let newQuantity = 1;
-  req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      let product;
-      if (products.length > 0) {
-        product = products[0];
-      }
+  Product.findById(prodId).then(product=>{
+    return req.user.addToCart(product);
+  }).then(result=>{
+    console.log(result);
+  });
+  // let fetchedCart;
+  // let newQuantity = 1;
+  // req.user
+  //   .getCart()
+  //   .then(cart => {
+  //     fetchedCart = cart;
+  //     return cart.getProducts({ where: { id: prodId } });
+  //   })
+  //   .then(products => {
+  //     let product;
+  //     if (products.length > 0) {
+  //       product = products[0];
+  //     }
 
-      if (product) {
-        const oldQuantity = product.cartItem.quantity;
-        newQuantity = oldQuantity + 1;
-        return product;
-      }
-      return Product.findById(prodId);
-    })
-    .then(product => {
-      return fetchedCart.addProduct(product, {
-        through: { quantity: newQuantity }
-      });
-    })
-    .then(() => {
-      res.redirect('/cart');
-    })
-    .catch(err => console.log(err));
+  //     if (product) {
+  //       const oldQuantity = product.cartItem.quantity;
+  //       newQuantity = oldQuantity + 1;
+  //       return product;
+  //     }
+  //     return Product.findById(prodId);
+  //   })
+  //   .then(product => {
+  //     return fetchedCart.addProduct(product, {
+  //       through: { quantity: newQuantity }
+  //     });
+  //   })
+  //   .then(() => {
+  //     res.redirect('/cart');
+  //   })
+  //    .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
--- models/user.js
+++ models/user.js
@@ -1,58 +1,75 @@
 const mongodb = require('mongodb');
+const ObjectId = mongodb.ObjectId;
 // getting access to the database
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart;    // {items: []}
+    this._id = id;
   }
   save() {
     // getting access to the database
     const db = getDb();
-    db.collection('users')
-      .insertOne(this)
-      // telling mongoDB in which collection you want to insert something
-      // if the collection does not exist, it will be created
-      .then(result=>{
-        console.log(result);
-      })
-      .catch (err => {
-      console.log(err);
-    });
+    db.collection('users').insertOne(this);
+    // telling mongoDB in which collection you want to insert something
+    // if the collection does not exist, it will be created
   }
-  static findUserById(userId) {
+
+  addToCart(product) {
+    // finding the item if it exists in the cart already
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    // newQuantity is 1 if the product is not present in cart
+    let newQuantity = 1;
+    // getting access to the cart 
+    const updatedCartItems = [...this.cart.items];
+    // cartProductIndex will be >=0 only if the product exists in cart
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      // updating the quantity for our product
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+
+    // we are storing an array named items in the cart 
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+
+  static findById(userId) {
     const db = getDb();
     // find will return a cursor as mongoDB won't know that I will only get one
     // next will give access to the next, i.e. the last document that was returned here
     return db
       .collection('users')
-      // mongodb stores the ids in the format of objectId so rather than just comparing the string we will compare the ObjectId
-      .find({ _id: new mongodb.ObjectId(userId) })
-      .next()
+      .findOne({ _id: new mongodb.ObjectId(userId) })
       .then(user => {
         console.log(user);
         return user;
       })
       .catch(err => {
         console.log(err);
-      })
+      });
+    // mongodb stores the ids in the format of objectId so rather than just comparing the string we will compare the ObjectId
+
   }
 }
-
-// const Sequelize = require('sequelize');
-
-// const sequelize = require('../util/database');
-
-// const User = sequelize.define('user', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   name: Sequelize.STRING,
-//   email: Sequelize.STRING
-// });
 
 module.exports = User;
